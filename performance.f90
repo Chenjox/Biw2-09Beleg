8724f9ccf26c82df71fa13354a60fe2c2d8e9767
Chenjox <chenjox.diepelt@gmx.de>
2022-04-27 23:53:34 +0200

    refining benchmark

--- performance.f90
+++ performance.f90
@@ -20,52 +20,49 @@ subroutine performance(maxGroesse, maxDurchlauf, dichteInkrement)
 
   ! maxDichte
   maxDichte = 100
+  zeiten = 0.0
   !if(maxDichte.eq.0) maxDichte = 100 / dichteInkrement
 
   open(unit=30,file='performance.csv')
   write(unit=30,fmt='(A34)') 'groesse,dichte,zeit1,zeit2,zeit3'
 
-  do groesse = 1, maxGroesse
-    write(*,*) groesse
-    allocate(matrix(groesse,groesse)) ! Die Matrix ist groesse x groesse
-    do dichte = dichteInkrement, maxDichte, dichteInkrement
-      write(*, *) dichte
+  do dichte = dichteInkrement, maxDichte, dichteInkrement
+    write(*, *) dichte
+    do groesse = 1, maxGroesse
+      write(*,*) groesse
+      allocate(matrix(groesse,groesse)) ! Die Matrix ist groesse x groesse
       do durchlauf=1, maxDurchlauf
         call zufallMatrix(matrix,groesse,dichte) ! Zufallige belegung der Matrix
         call cpu_time(startTime)
         call determinanteLaplace(matrix, groesse, determinant)
         call cpu_time(finishTime)
-        timeTaken = timeTaken + finishTime - startTime
-      end do
-      timeTaken = timeTaken / maxDurchlauf
-      zeiten(1) = timeTaken ! Schreiben in ein Array
-      ! TODO Hier kommt noch das schreiben in eine Datei
-      timeTaken = 0.0 ! Alle Variablen zurücksetzen!
-      do durchlauf=1, maxDurchlauf
-        call zufallMatrix(matrix,groesse,dichte) ! Zufallige belegung der Matrix
+        ! Wir berechnen es inplace
+        zeiten(1) = zeiten(1) + finishTime - startTime
+
         call cpu_time(startTime)
         call determinanteLaplaceMitAbsuchen(matrix, groesse, determinant)
         call cpu_time(finishTime)
-        timeTaken = timeTaken + finishTime - startTime
-      end do
-      timeTaken = timeTaken / maxDurchlauf
-      zeiten(2) = timeTaken
-      ! TODO Hier kommt noch das schreiben in eine Datei
-      timeTaken = 0.0 ! Alle Variablen zurücksetzen!
-      do durchlauf=1, maxDurchlauf
-        call zufallMatrix(matrix,groesse,dichte) ! Zufallige belegung der Matrix
+
+        zeiten(2) = zeiten(2) + finishTime - startTime
+
         call cpu_time(startTime)
         call determinanteDreiecksform(matrix, groesse, determinant)
         call cpu_time(finishTime)
-        timeTaken = timeTaken + finishTime - startTime
+
+        zeiten(3) = zeiten(3) + finishTime - startTime
+
       end do
-      timeTaken = timeTaken / maxDurchlauf
-      zeiten(3) = timeTaken
-      timeTaken = 0.0 ! Alle Variablen zurücksetzen!
+
+      zeiten(1) = zeiten(1)/maxDurchlauf
+      zeiten(2) = zeiten(2)/maxDurchlauf
+      zeiten(3) = zeiten(3)/maxDurchlauf
 
       write (30, '(I2,",",I3,3(",", F16.8))') groesse, dichte, zeiten(1), zeiten(2), zeiten(3)
+
+      zeiten = 0.0
+      ! Die Matrix muss freigegeben werden.
+      deallocate(matrix)
     end do
-    deallocate(matrix) ! Die Matrix muss freigegeben werden.
   end do
 
   close(unit=30)
