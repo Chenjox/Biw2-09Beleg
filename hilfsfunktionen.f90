f12f98b3fd29749961416a19b2308fc413890254
Jochen Diepelt <jochen.diepelt@mailbox.tu-dresden.de>
2022-05-12 14:26:11 +0200

    fixing hilfsfunktion.f90

--- hilfsfunktionen.f90
+++ hilfsfunktionen.f90
@@ -55,7 +55,7 @@ real :: Z(n,n)
       do k=1,n
           call random_number(u)    !Funktion erzeugt Pseudozufallszahl von 0 bis 1
           v = FLOOR(101*u)        !Zahlen von 0 bis 100, die Zufallszahl mal 101 erzeugt Zahl von 1-100, FLOOR() rundet ab
-          if(v.lt.dichte) then    !Falls die Zufallszahl kleiner als unsere geforderte Dichte, dann Erzeuge in der Zelle eine Null
+          if(v.gt.dichte) then    !Falls die Zufallszahl kleiner als unsere geforderte Dichte, dann Erzeuge in der Zelle eine Null
               Z(i,k)=0
           else                    !Sonst fülle die Zelle mit einer Zufallszahl
               call random_number(u)
